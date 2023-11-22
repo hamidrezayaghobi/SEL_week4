92b7710e167eec1bd4ab7f7f139129f17c54f92a
hamidrezayaghobi <hamidrezayaghobi021@gmail.com>
2023-11-22 19:57:10 +0330

    add state to DeliveryContext

--- src/main/DeliveredState.java
+++ src/main/DeliveredState.java
@@ -4,6 +4,5 @@ public class DeliveredState implements PackageState {
     @Override
     public void updateState(DeliveryContext ctx) {
         System.out.println("Package is delivered.");
-        // Ending the program or state transition logic
     }
 }
